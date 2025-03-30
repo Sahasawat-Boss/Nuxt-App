029b29b6f381ccd23c30ee7b75464343ccc8d21e
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-30 08:15:28 +0700

    Added Pages Routes Parameter

---
+++ pages/products/[id].vue
@@ -0,0 +1,31 @@
+<script setup>
+//http://localhost:3000/products/33
+const { id } = useRoute().params
+</script>
+
+
+<template>
+    <h2>Products id: {{ id }}</h2>
+    <p>
+        Details of product: {{ id }}
+    </p>
+    <p>
+        {{ id }} Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, natus.
+    </p>
+    <p>
+        {{ id }} Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, natus.
+    </p>
+</template>
+
+
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
