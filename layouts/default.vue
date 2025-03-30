b2bceb99aa147a06ff87b17c2b8512eb085e5df9
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-30 16:54:30 +0700

    Custom active-class

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,16 @@
     <main>
         <header class="shadow-sm">
             <nav class="container mx-auto p-4 flex justify-between">
-                <NuxtLink to="/" class="font-bold">Nuxt App Test</NuxtLink>
+                <NuxtLink to="/" class="font-bold" >Nuxt App Test</NuxtLink>
                 <ul class="flex gap-4">
                     <li>
-                        <NuxtLink to="/">Home</NuxtLink>
+                        <NuxtLink to="/" active-class="text-blue-500">Home</NuxtLink>
                     </li>
                     <li>
-                        <NuxtLink to="/about">About</NuxtLink>
+                        <NuxtLink to="/about" active-class="text-blue-500">About</NuxtLink>
                     </li>
                     <li>
-                        <NuxtLink to="/products" class="btn">Products</NuxtLink>
+                        <NuxtLink to="/products" class="btn" active-class="text-blue-200">Products</NuxtLink>
                     </li>
                 </ul>
             </nav>
@@ -27,7 +27,5 @@
 </template>
 
 <style scoped>
-.router-link-exact-active {
-    color: rgb(85, 193, 75);
-}
+
 </style>
--- layouts/products.vue
+++ layouts/products.vue
@@ -12,13 +12,13 @@
         <footer class="container mx-auto p-4 flex justify-between border-t-2">
             <ul class="flex gap-4">
                 <li>
-                    <NuxtLink to="/">Home</NuxtLink>
+                    <NuxtLink to="/" active-class="text-blue-500">Home</NuxtLink>
                 </li>
                 <li>
-                    <NuxtLink to="/about">About</NuxtLink>
+                    <NuxtLink to="/about" active-class="text-blue-500">About</NuxtLink>
                 </li>
                 <li>
-                    <NuxtLink to="/products">Merch</NuxtLink>
+                    <NuxtLink to="/products" class="btn" active-class="text-blue-200">Products</NuxtLink>
                 </li>
             </ul>
         </footer>
@@ -26,7 +26,4 @@
 </template>
 
 <style scoped>
-.router-link-exact-active {
-    color: rgb(85, 193, 75);
-}
 </style>
