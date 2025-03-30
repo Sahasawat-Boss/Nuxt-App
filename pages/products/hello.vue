f5ce7df1b237112ee1540fdb326845d12fb97a02
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-30 08:05:45 +0700

    Added Pages and Sub Pages

---
+++ pages/products/hello.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>Products: hello</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem eaque placeat illum ratione, porro quas ducimus
+    </p>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p {
+    margin: 20px 0;
+}
+</style>
